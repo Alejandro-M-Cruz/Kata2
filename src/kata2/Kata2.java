260e22e574a91b821d5f55d6ed5cc7fb82c0814e
DESKTOP-2AG9GJT\Alejandro Cruz <alejandroxdlx@gmail.com>
2022-09-29 09:30:20 +0100

    Tercer commit

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,49 +7,12 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        map.put(2,32);
-        map.put(7,42);
-        map.put(32,104);
-        map.put(5,25);
-        map.put(0,2);
-        
-        Iterator<Map.Entry<Integer,Integer>> iteratorEntries = map.entrySet().iterator();
-        
-        while(iteratorEntries.hasNext()) {
-            Map.Entry<Integer,Integer> entry = iteratorEntries.next();
-            System.out.println("Key = "+entry.getKey()+", Value = "+entry.getValue());
-        }
-
-        System.out.println("------------");
-
-        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            System.out.println("Key = "+entry.getKey()+", Value = "+entry.getValue());
-        }
-        
-        System.out.println("------------");
-        
-        for(Integer key : map.keySet()) {
-            System.out.println("Key = "+key+", Value = "+map.get(key));     // Menos eficiente, más accesos al mapa       
-        }
-                
-        System.out.println("------------");
-        
-        for(Integer value : map.values()) {
-            System.out.println("Value = "+value);     // Menos eficiente, más accesos al mapa       
-        }
-        
-        System.out.println("------------");
-        
         // Histograma
-        int[] array = {15,4,4,3,78,4,2,2,2,2,65,4,2,-1,2};
+        int[] data = {15,4,4,3,78,4,2,2,2,2,65,4,2,-1,2};
         
-        Map<Integer,Integer> histogram = new HashMap<Integer,Integer>();
-        for(int i : array) {
-            histogram.put(i, histogram.containsKey(i) ? histogram.get(i)+1 : 1);
-        }
+        HistogramGenerator histogramGenerator = new HistogramGenerator(data);
         
-        for(Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+        for(Map.Entry<Integer, Integer> entry : histogramGenerator.getHistogram().entrySet()) {
             System.out.println("Key = "+entry.getKey()+", Value = "+entry.getValue());
         }
     }
